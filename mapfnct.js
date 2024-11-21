f67789bacf6a128dab575c9ad99f6d8e0d63edb2
Mahima <128793921+mahima9728@users.noreply.github.com>
2024-11-21 13:52:01 +0530

    adding file

---
+++ mapfnct.js
@@ -0,0 +1,7 @@
+const friends = ['Alice', 'Bob', 'Charlie'];
+
+const pizzaOrders = friends.map(friend => `${friend}'s Pizza`);
+console.log(pizzaOrders);
+
+
+// Output: ["Alice's Pizza", "Bob's Pizza", "Charlie's Pizza"]
